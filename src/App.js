d4fc8338e7156108852561dd6ebe77e330b6f93f
pranavshukla1011 <shuklapranav1011@gmail.com>
2021-05-13 04:23:43 +0530

    render page

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import AboutLinks from './components/AboutLinks';
+
+import buildingSite from './images/building_website.svg';
+const App = () => {
+  return (
+    <AppContainer>
+      <Image src={buildingSite} alt='' />
+      <Content>
+        <h4>ERROR 404</h4>
+        <br />
+        <h2>Under development....I will launch this before 20th May, 2021</h2>
+        <AboutLinks></AboutLinks>
+      </Content>
+    </AppContainer>
+  );
+};
+
+const AppContainer = styled.div`
+  display: grid;
+  height: 100vh;
+  width: 100vw;
+  overflow: auto;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Roboto', sans-serif;
+`;
+
+const Image = styled.img`
+  justify-self: center;
+  width: 100%;
+  height: auto;
+`;
+
+const Content = styled.div`
+  justify-self: center;
+  > h4 {
+    text-align: center;
+  }
+`;
+export default App;
